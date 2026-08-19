dc35c430781ad9561aeff47295288be421d2c101
Tural V. Hasanov <turalvh@code.local>
2026-08-19 12:52:01 +0400

    Admin area

---
+++ Booksaw/Booksaw/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booksaw.Areas.Admin.Controllers
+{
+    
+    public class DashboardController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Booksaw/Booksaw/Program.cs
+++ Booksaw/Booksaw/Program.cs
@@ -17,6 +17,10 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
